968dafb41caa27415764a401ca9bf62d7ace8e2d
Ricardo Costa <ricardo.costa@wipro.com>
2016-08-22 12:34:56 +0100

    Updated CD script

---
+++ scripts/release.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import xml.etree.ElementTree as ET;
+import os;
+
+currentVersion = ET.parse(open('pom.xml')).getroot().find('{http://maven.apache.org/POM/4.0.0}version').text
+
+bintrayUrl = 'https://api.bintray.com/content/buildit/maven/confluence-maven-plugin/' + currentVersion + '/confluence-maven-plugin-' + currentVersion + '.jar?publish=1'
+os.system('curl -u ' + os.environ['BINTRAY_USERNAME'] + ':' + os.environ['BINTRAY_PASSWORD'] + ' -T target/*.jar "' + bintrayUrl + '"')
+
+decomposedVersion = currentVersion.split('.')
+
+majorVersion = decomposedVersion[0]
+minorVersion = decomposedVersion[1]
+patchVersion = decomposedVersion[2].split('-')[0]
+
+nextVersion = majorVersion + '.' + minorVersion + '.' + str(int(patchVersion) + 1)
+
+os.system('mvn -DnewVersion=' + nextVersion + ' versions:set versions:commit')
+os.system('git add pom.xml')
--- scripts/release_to_bintray.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import os;
-
-os.system(
-    'curl -u ' + os.environ['BINTRAY_USERNAME'] + ':' + os.environ['BINTRAY_PASSWORD'] +
-    ' -T target/*.jar "https://api.bintray.com/content/buildit/maven/confluence-maven-plugin/${pomVersion}/confluence-maven-plugin-${pomVersion}.jar?publish=1"'
