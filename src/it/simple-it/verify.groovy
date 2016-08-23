f36e044192a1ea4d54b820b51cdfee52c2715a81
Ricardo Costa <ricardo.costa@wipro.com>
2016-08-23 17:29:39 +0100

    Fail plugin execution when any IO operation fails

--- src/it/simple-it/verify.groovy
+++
@@ -1,6 +0,0 @@
-def logLines = new File(basedir, "build.log").findAll({ it.startsWith("[INFO] Checking if") })
-
-assert "[INFO] Checking if 'Parent Document' document exists in space 'AC'..." in logLines
-assert "[INFO] Checking if 'Bar Document' document exists in space 'AC'..." in logLines
-assert "[INFO] Checking if 'Foo Document' document exists in space 'AC'..." in logLines
-
