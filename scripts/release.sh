4c1d5eaad720048f7a18977cad43811e9935230f
Ricardo Costa <ricardo.costa@wipro.com>
2016-08-24 12:33:13 +0100

    Updated release script to be more informational

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,10 +8,12 @@ os.system('git remote set-url origin ' + os.environ['GITHUB_AUTH_REPO_URL'] + ' 
 os.system('git config --global push.default simple')
 
 # tag
+print('Tagging current version: ' + currentVersion)
 os.system('git -c user.name="travis" -c user.email="travis" tag -a ' + currentVersion + ' -m "[skip ci] Built version: ' + currentVersion + '"')
 os.system('git push --tags &> /dev/null')
 
 # bintray upload
+print('Uploading maven artifact to bintray')
 bintrayVersionUrl = 'https://api.bintray.com/maven/buildit/maven/confluence-maven-plugin/;publish=1/com/wiprodigital/confluence-maven-plugin/' + currentVersion + '/confluence-maven-plugin-' + currentVersion
 bintrayJarUrl = bintrayVersionUrl + '.jar'
 bintrayPomUrl = bintrayVersionUrl + '.pom'
@@ -27,6 +29,7 @@ patchVersion = decomposedVersion[2].split('-')[0]
 
 nextVersion = majorVersion + '.' + minorVersion + '.' + str(int(patchVersion) + 1)
 
+print('Bumping version to: ' + nextVersion)
 os.system('git checkout master &> /dev/null')
 os.system('mvn -DnewVersion=' + nextVersion + ' versions:set versions:commit')
 os.system('git add pom.xml')
