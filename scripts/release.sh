55f8828c9a13d6b0c453f2f49f695d915faeed5b
Ricardo Costa <ricardo.costa@wipro.com>
2016-08-24 12:59:04 +0100

    Debugging release script in travici

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,9 +3,10 @@
 import xml.etree.ElementTree as ET;
 import os;
 
-currentVersion = ET.parse(open('pom.xml')).getroot().find('{http://maven.apache.org/POM/4.0.0}version').text
 os.system('git remote set-url origin ' + os.environ['GITHUB_AUTH_REPO_URL'] + ' &> /dev/null')
 os.system('git config --global push.default simple')
+os.system('git checkout master &> /dev/null')
+currentVersion = ET.parse(open('pom.xml')).getroot().find('{http://maven.apache.org/POM/4.0.0}version').text
 
 # tag
 print('Tagging current version: ' + currentVersion)
@@ -30,7 +31,6 @@ patchVersion = decomposedVersion[2].split('-')[0]
 nextVersion = majorVersion + '.' + minorVersion + '.' + str(int(patchVersion) + 1)
 
 print('Bumping version to: ' + nextVersion)
-os.system('git checkout master &> /dev/null')
 os.system('mvn -DnewVersion=' + nextVersion + ' versions:set versions:commit')
 os.system('git add pom.xml')
 os.system('git -c user.name="travis" -c user.email="travis" commit -m "[skip ci] Bumping version to ' + nextVersion + '"')
