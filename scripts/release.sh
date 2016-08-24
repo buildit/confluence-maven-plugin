b0aadff017609a424d26771e29a16aeccb0c53f7
Ricardo Costa <ricardo.costa@wipro.com>
2016-08-24 13:36:26 +0100

    Updated release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,13 +3,21 @@
 import xml.etree.ElementTree as ET;
 import os;
 
+os.system('git remote set-url origin ' + os.environ['GITHUB_AUTH_REPO_URL'] + ' &> /dev/null')
+os.system('git config --global push.default simple')
+os.system('git config --global user.name travis')
+os.system('git config --global user.email travis')
+os.system('git checkout master &> /dev/null')
+
 currentVersion = ET.parse(open('pom.xml')).getroot().find('{http://maven.apache.org/POM/4.0.0}version').text
 
 # tag
-os.system('git -c user.name="travis" -c user.email="travis" tag -a ' + currentVersion + ' -m "[skip ci] Built version: ' + currentVersion + '"')
-os.system('git push --tags --repo ' + os.environ['GITHUB_AUTH_REPO_URL'] + ' &> /dev/null')
+print('Tagging current version: ' + currentVersion)
+os.system('git tag -a ' + currentVersion + ' -m "[skip ci] Built version: ' + currentVersion + '" &> /dev/null')
+os.system('git push --tags &> /dev/null')
 
 # bintray upload
+print('Uploading maven artifact to bintray')
 bintrayVersionUrl = 'https://api.bintray.com/maven/buildit/maven/confluence-maven-plugin/;publish=1/com/wiprodigital/confluence-maven-plugin/' + currentVersion + '/confluence-maven-plugin-' + currentVersion
 bintrayJarUrl = bintrayVersionUrl + '.jar'
 bintrayPomUrl = bintrayVersionUrl + '.pom'
@@ -25,8 +33,13 @@ patchVersion = decomposedVersion[2].split('-')[0]
 
 nextVersion = majorVersion + '.' + minorVersion + '.' + str(int(patchVersion) + 1)
 
-os.system('git checkout ' + os.environ['TRAVIS_BRANCH'] + ' &> /dev/null')
+print('Bumping version to: ' + nextVersion)
 os.system('mvn -DnewVersion=' + nextVersion + ' versions:set versions:commit')
 os.system('git add pom.xml')
-os.system('git -c user.name="travis" -c user.email="travis" commit -m "[skip ci] Bumping version to ' + nextVersion + '"')
-os.system('git push --repo ' + os.environ['GITHUB_AUTH_REPO_URL'] + ' &> /dev/null')
+os.system('git commit -m "[skip ci] Bumping version to: ' + nextVersion + '"')
+os.system('git push -q')
+
+
+
+
+
