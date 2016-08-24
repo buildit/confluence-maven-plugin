85eb1cb4cfbdfbe85b429bc579f1207b98289e29
Ricardo Costa <ricardo.costa@wipro.com>
2016-08-24 13:12:00 +0100

    Debugging release script in travici

--- scripts/release.sh
+++ scripts/release.sh
@@ -34,6 +34,8 @@ print('Bumping version to: ' + nextVersion)
 os.system('mvn -DnewVersion=' + nextVersion + ' versions:set versions:commit')
 os.system('git add pom.xml')
 os.system('git -c user.name="travis" -c user.email="travis" commit -m "[skip ci] Bumping version to ' + nextVersion + '"')
-os.system('git push')
+os.system('git push &> /dev/null')
+
+
 
 
